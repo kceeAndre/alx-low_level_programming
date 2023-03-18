12151bed2eef2b282ce5a3c9c1e280cec21a22eb
Jephthah <nwakwuekelechukwu@gmail.com>
2023-03-18 20:00:24 +0000

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+* main - this is the main
+*
+* Return: to return zero
+**/
+
+#include <stdio.h>
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle);
+return(0);
+}
