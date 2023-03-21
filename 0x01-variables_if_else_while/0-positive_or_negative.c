5626f9b9d8c65131d9e06cdbd84d983b5d5bbd23
Jephthah <nwakwuekelechukwu@gmail.com>
2023-03-21 09:31:24 +0000

    task 0 done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+/**
+*main - "check if n is +ve or -ve"
+*Return: return (0)
+*/
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+puts("is positive");
+}
+else if (n == 0)
+{
+puts("is zero");
+}
+else
+{
+puts("is negative");
+}
+return (0);
+}
