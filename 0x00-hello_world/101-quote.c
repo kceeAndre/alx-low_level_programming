86d8b445583c59c158cf00a0ec1c9588cd5af59f
Jephthah <nwakwuekelechukwu@gmail.com>
2023-03-21 06:53:06 +0000

    100 and 101 task done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+/**
+*main - this is the main for 101 quote
+*Return: this will return 1
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
