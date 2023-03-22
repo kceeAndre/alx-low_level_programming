1c8245c8e992b6e6e6a6e74dccb5e5da937d3d8c
Jephthah <nwakwuekelechukwu@gmail.com>
2023-03-22 18:09:42 +0000

    correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-/**                                           *main - "print numbers"
-*Return: return (0)
+/**
+*main - " print numbers"
+*Return: return 0
 */
 
-int main(void)                                {
+int main(void)
+{
 int i;
 for (i = '0'; i <= '9'; i++)
 {
