ae8f6a637c49eda9312219b4952da863c0f13d08
Jephthah <nwakwuekelechukwu@gmail.com>
2023-03-24 14:30:59 +0000

    me

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - "entry point"
+*Return: return 0
+*/
+
+int main(void)
+{
+int i;
+printf("infinite loop incoming :(\n");
+i = 0;
+
+/**
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("Infinte loop avoided! \\o/\n");
+return (0);
+}
